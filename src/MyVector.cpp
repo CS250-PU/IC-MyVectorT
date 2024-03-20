aa1253cc587c23dac97f8b3228e4382ecc5bdac8
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2024-03-20 17:27:53 +0000

    MyVector class template

---
+++ src/MyVector.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "../include/MyVector.h"
+
+using namespace std;
--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,48 @@
 //******************************************************************************
 
 #include <iostream>
+#include "../include/MyVector.h"
 
 using namespace std;
 
 int main () {
+  //TODO #1: Implement constructor
+  // MyVector cMyVector1, cMyVector2 (20);
+
+  cout << "*** MyVector Dynamic Implementation ***" << endl << endl;
+
+  //TODO #2: Implement capacity and size functions
+  // cout << "Capacity of cMyVector1: " << cMyVector1.capacity () << endl;
+  // cout << "Capacity of cMyVector2: " << cMyVector2.capacity () << endl << endl;
+
+  // cout << "Size of cMyVector1: " << cMyVector1.size () << endl;
+  // cout << "Size of cMyVector2: " << cMyVector2.size () << endl << endl;
+
+  //TODO #3: Implement push_back
+  // cMyVector1.push_back (1);
+  // cMyVector1.push_back (2);
+  // cout << "Size of cMyVector1: " << cMyVector1.size () << endl;
+  // cout << "Size of cMyVector2: " << cMyVector2.size () << endl << endl;
+
+  //TODO #4: Implement at
+  // for (unsigned int i = 0; i < cMyVector1.size (); i++) {
+  //   cout << "cMyVector1[" << i << "]: " << cMyVector1.at (i) << endl;
+  // }
+  // cout << endl;
+
+  //TODO #5: Final test
+  // for (int i = 10; i < 20; i++) {
+  //   cMyVector1.push_back (i);
+  // }
+
+  // for (unsigned int i = 0; i < cMyVector1.size (); i++) {
+  //   cout << "cMyVector1[" << i << "]: " << cMyVector1.at (i) << endl;
+  // }
+
+  //TODO #6: Type "make valgrind" without the quotes in the terminal. 
+  //         Any ideas what's going on?
+
+  cout << endl << "Processing Complete" << endl;
 
   return EXIT_SUCCESS;
 }
